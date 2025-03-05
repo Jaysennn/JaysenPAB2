02af17f2def2bf940ecfa638ca68091d72e6390c
Jaysennn <jaysenstephanus_2327250042@mhs.mdp.ac.id>
2025-03-05 08:19:54 +0700

    tes

--- film/lib/main.dart
+++ film/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:film/screens/favorite_screen.dart';
 import 'package:film/screens/home_screen.dart';
+import 'package:film/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +13,28 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen()
+      home: MainScreen()
       );
+  }
+}
 
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
